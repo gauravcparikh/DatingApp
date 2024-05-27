e7b2817e068c91f2478677958720175df5cc5c2a
Gourav Parikh <gauravcparikh@gmail.com>
2024-05-27 14:38:22 +0530

    section 7

--- API/Program.cs
+++ API/Program.cs
@@ -9,21 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// // builder.Services.AddDbContext<DataContext>(opt =>
-// // {
-// //     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-// // });
-// builder.Services.AddCors();
-// // builder.Services.AddScoped<ITokenService, TokenService>();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
-
+app.UseMiddleware<ExceptionMiddleware>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
